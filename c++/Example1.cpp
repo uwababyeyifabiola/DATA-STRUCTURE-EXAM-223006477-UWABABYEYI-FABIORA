13962ef7f01501286f2c9d0d10ada2a6dc606477
uwababyeyifabiola <uwababyeyifabiola@gmail.com>
2025-02-21 10:37:12 +0400

    Add files via upload

---
+++ c++/Example1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // Include input-output library
+using namespace std; // Use standard namespace
+
+int main() {  // Main function
+    cout << "Hello, World!";  // Print output to the screen
+    return 0;  // Return 0 to indicate successful execution
+}
+
+
